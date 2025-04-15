fd13c13a02987ddca6f580650b3a1e7e33c423ab
Joel Vasiliev <joel.vasiliev.neto@gmail.com>
2025-04-15 01:46:39 -0300

    rm unused files

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { findSimilarProposals } from '@/actions/find-similar-proposals';
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-
-    const data = await findSimilarProposals();
-
-    return NextResponse.json([...data], { status: 200 });
-}
